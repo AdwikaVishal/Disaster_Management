6a2cddcba826a653ab28865ceeda165ea5262ea9
apple <adwikavishal@rediffmail.com>
2025-12-28 23:56:28 +0530

    Resolve env merge conflict

--- backend/src/main/java/com/sensesafe/SenseSafeApplication.java
+++ backend/src/main/java/com/sensesafe/SenseSafeApplication.java
@@ -11,18 +11,6 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @EnableAsync
 @EnableScheduling
 public class SenseSafeApplication {
-    @org.springframework.context.annotation.Bean
-    public org.springframework.web.servlet.config.annotation.WebMvcConfigurer corsConfigurer() {
-        return new org.springframework.web.servlet.config.annotation.WebMvcConfigurer() {
-            @Override
-            public void addCorsMappings(org.springframework.web.servlet.config.annotation.CorsRegistry registry) {
-                registry.addMapping("/**")
-                        .allowedOrigins("*")
-                        .allowedMethods("*")
-                        .allowedHeaders("*");
-            }
-        };
-    }
 
     public static void main(String[] args) {
         SpringApplication.run(SenseSafeApplication.class, args);
